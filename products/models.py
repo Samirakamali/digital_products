ed756dd51c501b53d3165dab3c27f0ba8309c2d8
Samira Kamali Poorazad <139060667+Samirakamali@users.noreply.github.com>
2025-06-04 19:50:45 +0300

    Update models.py

--- products/models.py
+++ products/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _  # for supporting persian language or translation from english
+from django.utils.translation import gettext_lazy as _ 
 
 # Create your models here.
 class Category(models.Model):
-    parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE)  # baraye subcategory b khodesh foreinkey mizanam. va inke agar category parent hazf shod badesh che etefaqi biofte
-    title = models.CharField(_('title'), max_length=50)  # the first arguman is for verbose_name(what I expect be showun as name of product rathe than its saved name in database!)
+    parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE) 
+    title = models.CharField(_('title'), max_length=50)  
     description = models.TimeField(_('description'), blank=True)
     avatar = models.ImageField(_('avatar'), blank=True, upload_to='categories')
     is_enable = models.BooleanField(_('is enable'), default=True)
@@ -12,8 +12,8 @@ class Category(models.Model):
     updated_time = models.DateTimeField(_('updated time'), auto_now=True)
 
     class Meta:  # baraye ezafe kardan vizegihaye ezafeh
-        db_table = 'categories'  # ino mizara, ta esme jadvalmo tu database khodam malum konam vagarna default esme app_esme model mishe!
-        verbose_name = _('Category')  # esme categori k admin mibine
+        db_table = 'categories'  
+        verbose_name = _('Category') 
         verbose_name_plural = _('Categories')
 
 
@@ -22,7 +22,7 @@ class Product(models.Model):
     description = models.TimeField(_('description'), blank=True)
     avatar = models.ImageField(_('avatar'), blank=True,upload_to='categories')
     is_enable = models.BooleanField(_('is enable'), default=True)
-    categories = models.ManyToManyField('Category', verbose_name=_('categories'), blank=True)# many to many yani yeki az productha mitune jozi bishtar az yek categori bashe
+    categories = models.ManyToManyField('Category', verbose_name=_('categories'), blank=True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time'), auto_now=True)
 
@@ -33,7 +33,7 @@ class Product(models.Model):
 
 
 class File(models.Model):
-    product = models.ForeignKey('Product', verbose_name=_('product'), blank=True, null=True, on_delete=models.CASCADE) # chanta file b yek product vasl bshan. baraye hamin az ravesh forienkey estafade mikonim
+    product = models.ForeignKey('Product', verbose_name=_('product'), blank=True, null=True, on_delete=models.CASCADE) 
     title = models.CharField(_('title'), max_length=50)
     description = models.TimeField(_('description'), blank=True)
     file = models.FileField(_('file'), upload_to='files/%Y/%m/%d/') #taeen sal mah ruz
