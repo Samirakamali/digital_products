891708c6f345e9e8cf8f15ff8407a3404228bf7d
samira <139060667+SamiraKamlai@users.noreply.github.com>
2025-06-10 00:05:41 +0300

    changed confilict

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Category, Product, File
 
 # Register your models here.
+
+@admin.register(Category)
+class categoryAdmin(admin.ModelAdmin):
+    list_display = ['parent', 'title', 'is_enable', 'created_time']
+    list_filter = ['is_enable', 'parent']
+    search_fields = ['title']
+
+
+
+class FileInLineAdmin(admin.StackedInline):
+    model = File
+    feilds = ['title','file', 'is_enable']
+    extra = 0
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'is_enable', 'created_time']
+    list_filter = ['is_enable']
+    search_fields = ['title']
+    filter_horizontal = ['categories']
+    inlines = [FileInLineAdmin]
+
+
--- products/models.py
+++ products/models.py
@@ -3,9 +3,15 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Category(models.Model):
+<<<<<<< HEAD
     parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE) 
     title = models.CharField(_('title'), max_length=50)  
     description = models.TimeField(_('description'), blank=True)
+=======
+    parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE)  # baraye subcategory b khodesh foreinkey mizanam. va inke agar category parent hazf shod badesh che etefaqi biofte
+    title = models.CharField(_('title'), max_length=50)  # the first arguman is for verbose_name(what I expect be showun as name of product rathe than its saved name in database!)
+    description = models.TextField(_('description'), blank=True)
+>>>>>>> 6831b5c (models added to admin.)
     avatar = models.ImageField(_('avatar'), blank=True, upload_to='categories')
     is_enable = models.BooleanField(_('is enable'), default=True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
@@ -16,10 +22,13 @@ class Category(models.Model):
         verbose_name = _('Category') 
         verbose_name_plural = _('Categories')
 
+    def __str__(self):
+        return self.title 
+
 
 class Product(models.Model):
     title = models.CharField(_('title'), max_length=50)
-    description = models.TimeField(_('description'), blank=True)
+    description = models.TextField(_('description'), blank=True)
     avatar = models.ImageField(_('avatar'), blank=True,upload_to='categories')
     is_enable = models.BooleanField(_('is enable'), default=True)
     categories = models.ManyToManyField('Category', verbose_name=_('categories'), blank=True)
@@ -35,8 +44,13 @@ class Product(models.Model):
 class File(models.Model):
     product = models.ForeignKey('Product', verbose_name=_('product'), blank=True, null=True, on_delete=models.CASCADE) 
     title = models.CharField(_('title'), max_length=50)
+<<<<<<< HEAD
     description = models.TimeField(_('description'), blank=True)
     file = models.FileField(_('file'), upload_to='files/%Y/%m/%d/')
+=======
+    description = models.TextField(_('description'), blank=True)
+    file = models.FileField(_('file'), upload_to='files/%Y/%m/%d/') #taeen sal mah ruz
+>>>>>>> 6831b5c (models added to admin.)
     is_enable = models.BooleanField(_('is enable'), default=True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time'), auto_now=True)
