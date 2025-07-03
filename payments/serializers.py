eed566955d458130a229872208bfbf803fbea2b8
samira <139060667+SamiraKamlai@users.noreply.github.com>
2025-07-03 22:59:47 +0300

    payment view added

---
+++ payments/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Gateway
+
+class GatewaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Gateway
+        fields = ('id', 'title', 'description', 'avatar')
